655477d45cc66e39abb65294d8e8c665984e9c01
ramonGonzEdu <rg2872079@gmail.com>
2022-02-26 18:10:52 -0500

    Basic Player Controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    public float speed = 10.0f;
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    Vector2 inputDir = new Vector2(0, 0);
+
+    private void FixedUpdate()
+    {
+
+        // Handling flipping character during left/right movement
+        if (inputDir.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (inputDir.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        body.AddForce(inputDir * speed, ForceMode2D.Force);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // Using new InputSystem and Rigidbody2D for more interesting/intuitive movement
+        inputDir = context.ReadValue<Vector2>();
+    }
+}
